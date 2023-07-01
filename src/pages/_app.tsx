4d4bad98bf1434b7a33603ad181424a3bfcb8c48
kamalika0363 <59131714+doesnots@users.noreply.github.com>
2023-07-01 15:33:10 +0530

    Temporary Files added

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,12 @@
+import { type AppType } from "next/app";
 import { type Session } from "next-auth";
 import { SessionProvider } from "next-auth/react";
-import { type AppType } from "next/app";
+
 import { api } from "~/utils/api";
+
 import "~/styles/globals.css";
+import Head from "next/head";
+import { SideNav } from "~/components/SideNav";
 
 const MyApp: AppType<{ session: Session | null }> = ({
   Component,
@@ -10,7 +14,21 @@ const MyApp: AppType<{ session: Session | null }> = ({
 }) => {
   return (
     <SessionProvider session={session}>
-      <Component {...pageProps} />
+      <Head>
+        <title>T3-App</title>
+        <meta
+          name="description"
+          content="T3-App with Next.js, Tailwind CSS, and TypeScript"
+          // Started="20-6-2023"
+        />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <div className="container mx-auto flex items-start sm:pr-4">
+        <SideNav />
+        <div className="min-h-screen flex-grow border-x">
+          <Component {...pageProps} />
+        </div>
+      </div>
     </SessionProvider>
   );
 };
